a2158b42d35e9f91bb807a19fb8989b7d7153143
Innei <tukon479@gmail.com>
2026-08-27 05:29:08 +0800

    fix(mobile): compile rich heading styles as StyleX objects

--- packages/rich-content/src/lexical/portable/platform-icons.tsx
+++ packages/rich-content/src/lexical/portable/platform-icons.tsx
@@ -1,3 +1,5 @@
+import { sx } from '../../lib/sx'
+import { atoms } from '../../styles/atoms.stylex'
 import type { ReactNode, SVGProps } from 'react'
 
 function GitHubBrandIcon(props: SVGProps<SVGSVGElement>) {
@@ -10,7 +12,7 @@ function GitHubBrandIcon(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        className="fill-neutral-10"
+        {...sx(atoms.fill_neutral_10)}
         clipRule="evenodd"
         d="M7.5 0C3.35625 0 0 3.35625 0 7.5C0 10.8187 2.14687 13.6219 5.12812 14.6156C5.50312 14.6813 5.64375 14.4563 5.64375 14.2594C5.64375 14.0813 5.63438 13.4906 5.63438 12.8625C3.75 13.2094 3.2625 12.4031 3.1125 11.9812C3.02812 11.7656 2.6625 11.1 2.34375 10.9219C2.08125 10.7812 1.70625 10.4344 2.33438 10.425C2.925 10.4156 3.34688 10.9687 3.4875 11.1937C4.1625 12.3281 5.24063 12.0094 5.67188 11.8125C5.7375 11.325 5.93437 10.9969 6.15 10.8094C4.48125 10.6219 2.7375 9.975 2.7375 7.10625C2.7375 6.29062 3.02813 5.61562 3.50625 5.09062C3.43125 4.90312 3.16875 4.13437 3.58125 3.10312C3.58125 3.10312 4.20938 2.90625 5.64375 3.87188C6.24375 3.70313 6.88125 3.61875 7.51875 3.61875C8.15625 3.61875 8.79375 3.70313 9.39375 3.87188C10.8281 2.89688 11.4563 3.10312 11.4563 3.10312C11.8688 4.13437 11.6063 4.90312 11.5313 5.09062C12.0094 5.61562 12.3 6.28125 12.3 7.10625C12.3 9.98437 10.5469 10.6219 8.87813 10.8094C9.15 11.0437 9.38438 11.4938 9.38438 12.1969C9.38438 13.2 9.375 14.0063 9.375 14.2594C9.375 14.4563 9.51563 14.6906 9.89063 14.6156C12.8531 13.6219 15 10.8094 15 7.5C15 3.35625 11.6438 0 7.5 0Z"
         fillRule="evenodd"
@@ -193,49 +195,49 @@ const isXiaoHongShuUrl = (url: URL) => url.hostname.includes('xiaohongshu.com')
 const platformMap = [
   {
     type: 'GH',
-    icon: <GitHubBrandIcon className="text-[#1D2127] dark:text-[#FFFFFF]" />,
+    icon: <GitHubBrandIcon {...sx(atoms.text__hash1D2127, atoms.dark_text__hashFFFFFF)} />,
     test: isGithubUrl,
   },
   { type: 'TW', icon: <TwitterIcon />, test: isTwitterUrl },
   {
     type: 'TG',
-    icon: <TelegramIcon className="text-[#2AABEE]" />,
+    icon: <TelegramIcon {...sx(atoms.text__hash2AABEE)} />,
     test: isTelegramUrl,
   },
   {
     type: 'BL',
-    icon: <BilibiliIcon className="text-[#469ECF]" />,
+    icon: <BilibiliIcon {...sx(atoms.text__hash469ECF)} />,
     test: isBilibiliUrl,
   },
   {
     type: 'ZH',
-    icon: <ZhihuIcon className="text-[#0084FF]" />,
+    icon: <ZhihuIcon {...sx(atoms.text__hash0084FF)} />,
     test: isZhihuUrl,
   },
   {
     type: 'WI',
-    icon: <WikipediaIcon className="text-current" />,
+    icon: <WikipediaIcon {...sx(atoms.text_current)} />,
     test: isWikipediaUrl,
   },
   {
     type: 'TMDB',
-    icon: <TheMovieDBIcon className="text-[#0D243F] dark:text-[#5CB7D2]" />,
+    icon: <TheMovieDBIcon {...sx(atoms.text__hash0D243F, atoms.dark_text__hash5CB7D2)} />,
     test: isTMDBUrl,
   },
   {
     type: 'Moz',
-    icon: <MozillaIcon className="text-[#8cb4ff]" />,
+    icon: <MozillaIcon {...sx(atoms.text__hash8cb4ff)} />,
     test: isMozillaUrl,
   },
   { type: 'Npm', icon: <NpmIcon />, test: isNpmUrl },
   {
     type: 'Figma',
-    icon: <FigmaIcon className="text-[#A259FF]" />,
+    icon: <FigmaIcon {...sx(atoms.text__hashA259FF)} />,
     test: isFigmaUrl,
   },
   {
     type: 'XHS',
-    icon: <XiaoHongShuIcon className="text-[#FE2442]" />,
+    icon: <XiaoHongShuIcon {...sx(atoms.text__hashFE2442)} />,
     test: isXiaoHongShuUrl,
   },
 ] as const
