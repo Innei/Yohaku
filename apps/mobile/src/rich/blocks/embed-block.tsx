b4bf7b96cf7da906753cf706ceb9e6b13d7e5b18
Innei <innei.dev@gmail.com>
2026-09-23 17:17:18 +0800

    feat(mobile): render github-file embeds natively

--- apps/mobile/src/rich/blocks/embed-block.tsx
+++ apps/mobile/src/rich/blocks/embed-block.tsx
@@ -1,13 +1,22 @@
 import { UnsupportedBlock } from './card-blocks'
+import { parseGithubFileUrl } from './github-file'
+import { GithubFileBlock } from './github-file-block'
 import { TweetBlock } from './tweet-block'
 import { type BlockProps, str } from './types'
 
 export function EmbedBlock(props: BlockProps) {
-  switch (str(props.node.source)) {
+  const source = str(props.node.source)
+  switch (source) {
     case 'tweet': {
       return <TweetBlock {...props} />
     }
+    case 'github-file': {
+      return <GithubFileBlock {...props} />
+    }
     default: {
+      if (!source && parseGithubFileUrl(str(props.node.url))) {
+        return <GithubFileBlock {...props} />
+      }
       return <UnsupportedBlock {...props} />
     }
   }
